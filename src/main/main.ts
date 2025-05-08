e5107d209529a876b56103d942d0a6799a905a04
nolongerhesitate <nolongerstay@hotmail.com>
2025-05-08 22:23:03 +0800

    update: main.ts

--- src/main/main.ts
+++ src/main/main.ts
@@ -2,6 +2,9 @@ import { app, BrowserWindow } from 'electron';
 import path from 'node:path';
 import started from 'electron-squirrel-startup';
 
+declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string;
+declare const MAIN_WINDOW_VITE_NAME: string;
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
   app.quit();
